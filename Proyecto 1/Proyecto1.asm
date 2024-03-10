7317f8ebdc2a78cbef9c9fc4868d6d1951d57f66
Layla Lucía Blanco <99376557+Artis12@users.noreply.github.com>
2024-03-10 16:28:22 -0600

    Update Proyecto1.asm

--- Proyecto 1/Proyecto1.asm
+++ Proyecto 1/Proyecto1.asm
@@ -1,17 +1,17 @@
-%include "linux64.inc"
+%include "linux64.inc" ; Se usa un archivo externo para poder leer e imprimir el archivo .txt de NOTAS
 
 section .data
-	filename db "NOTAS.txt",0
+	filename db "NOTAS.txt",0 ; Se define el nombre del archivo que se va a leer 
 	
 section .bss
-	text resb 300
+	text resb 300 ; Se define el largo del archivo
 	
 section .text
 	global _start
 	
 _start:
 ; Open the file
-	mov rax, SYS_OPEN
+	mov rax, SYS_OPEN ; Abre el archivo
 	mov rdi, filename
 	mov rsi, O_RDONLY
 	mov rdx, 0
@@ -20,7 +20,7 @@ _start:
 ;read from the file
 	push rax
 	mov rdi, rax
-	mov rax, SYS_READ
+	mov rax, SYS_READ ; Lectura del archivo 
 	mov rsi, text
 	mov rdx, 300
 	syscall
@@ -29,5 +29,5 @@ _start:
 	mov rax, SYS_CLOSE
 	pop rdi
 	syscall
-	print text
+	print text ; Imprime el archivo 
 	exit
